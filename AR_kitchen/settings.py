35c4507266bcba62637a5d6ba8a588ddec5dc56d
ar210038 <al-rafi210038@diit.edu.bd>
2025-11-08 12:36:02 +0600

    Initial commit

--- AR_kitchen/settings.py
+++ AR_kitchen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ratc8&i9yg%e6sg^fah76*4jzc+dbu#k016lzotxirih37nk$%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
